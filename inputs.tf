fdda6b5541d761f9104da8ec88de5c6ef5272ce0
Christopher Childress <chris.childress@stratusgrid.com>
2022-12-14 15:45:14 -0500

    style: refine variable description

--- inputs.tf
+++ inputs.tf
@@ -1,6 +1,6 @@
 variable "enable_sns" {
   type        = bool
-  description = "Enable or disable SNS"
+  description = "Enable or disable GuardDuty findings notifications."
   default     = true
 }
 
