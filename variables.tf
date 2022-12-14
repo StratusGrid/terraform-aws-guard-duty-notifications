de184cf0c2f4f2cb4824cc4c0d447ee0ad01cc20
Christopher Childress <chris.childress@stratusgrid.com>
2022-12-14 16:44:47 -0500

    feat: add resource tagging

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
+variable "input_tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
 variable "enable_sns" {
   type        = bool
   description = "Enable or disable GuardDuty findings notifications."
   default     = true
 }
-
